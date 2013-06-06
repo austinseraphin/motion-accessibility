51ac43f0003765095d4e8a8870df25d4f16e23b9
Austin Seraphin <omeron@clearwhitelight.org>
2013-06-06 14:38:30 -0400

    Rewrote the attributes to use method_added

--- lib/project/attributes.rb
+++ lib/project/attributes.rb
@@ -1,29 +1,39 @@
 class UIView
 
-alias :accessibility_label :accessibilityLabel
-alias :accessibility_label= :setAccessibilityLabel
-alias :accessibility_hint :accessibilityHint
-alias :accessibility_hint= :setAccessibilityHint
-alias :accessibility_traits :accessibilityTraits
-alias :accessibility_value :accessibilityValue
-alias :accessibility_value= :setAccessibilityValue
-alias :accessibility_language :accessibilityLanguage
-alias :accessibility_language= :setAccessibilityLanguage
-alias :accessibility_frame :accessibilityFrame
-alias :accessibility_frame= :setAccessibilityFrame
-alias :accessibility_activation_point :accessibilityActivationPoint
-alias :accessibility_activation_point= :setAccessibilityActivationPoint
-alias :accessibility_view_is_modal :accessibilityViewIsModal
-alias :accessibility_view_is_modal= :setAccessibilityViewIsModal
-alias :should_group_accessibility_children :shouldGroupAccessibilityChildren
-alias :should_group_accessibility_children= :setShouldGroupAccessibilityChildren
-alias :accessibility_elements_hidden :accessibilityElementsHidden
-alias :accessibility_elements_hidden= :setAccessibilityElementsHidden
+Accessibility_Attributes = {
+:accessibility_label => :accessibilityLabel,
+:accessibility_label= =>  :setAccessibilityLabel,
+:accessibility_hint => :accessibilityHint,
+:accessibility_hint= => :setAccessibilityHint,
+:accessibility_traits => :accessibilityTraits,
+:accessibility_value => :accessibilityValue,
+:accessibility_value= => :setAccessibilityValue,
+:accessibility_language => :accessibilityLanguage,
+:accessibility_language= => :setAccessibilityLanguage,
+:accessibility_frame => :accessibilityFrame,
+:accessibility_frame= => :setAccessibilityFrame,
+:accessibility_activation_point => :accessibilityActivationPoint,
+:accessibility_activation_point= => :setAccessibilityActivationPoint,
+:accessibility_view_is_modal => :accessibilityViewIsModal,
+:accessibility_view_is_modal= => :setAccessibilityViewIsModal,
+:should_group_accessibility_children => :shouldGroupAccessibilityChildren,
+:should_group_accessibility_children= => :setShouldGroupAccessibilityChildren,
+:accessibility_elements_hidden => :accessibilityElementsHidden,
+:accessibility_elements_hidden= => :setAccessibilityElementsHidden
+}
 
+Accessibility_Attributes.each do |ruby, ios|
+next if ruby==:accessibility_traits=
+if ruby=~/=$/
+define_method(ruby) {|value| self.send(ios,value)}
+else
+define_method(ruby) {self.send(ios)}
+end
+end
 
 def accessibility_traits=(traits)
 bits=0
-if traits.kind_of?(Fixnum
+if traits.kind_of?(Fixnum)
 bits=traits
 elsif traits.kind_of?(Symbol)
 bits=traits.accessibility_trait
@@ -35,5 +45,27 @@ end
 self.accessibilityTraits=bits
 end
 
+if self.respond_to?(:method_added)
+alias :method_added_accessibility :method_added
 end
 
+def self.method_added(name)
+if self.respond_to?(:method_added_accessibility)
+method_added_accessibility(name)
+end
+return if name=~/=$/
+return unless Accessibility_Attributes.flatten.grep(%r{name.to_sym})
+if Accessibility_Attributes.has_key?(name)
+ruby=name
+ios=Accessibility_Attributes[name]
+NSLog("Redefining #{ios}")
+define_method(ios) {self.send(ruby)}
+else
+ios=name
+ruby=Accessibility_Attributes.rassoc(name).first
+NSLog("Redefining #{ruby}")
+define_method(ruby) { self.send(ios)}
+end
+end
+
+end
