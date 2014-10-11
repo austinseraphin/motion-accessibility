b040bd8a53905cf7cb699806b52c87f153d967a6
Austin Seraphin <omeron@clearwhitelight.org>
2014-10-11 17:03:18 -0400

    Moved the container test to its own helper

---
+++ spec/helpers/container.rb
@@ -0,0 +1,30 @@
+class Container_Test < UIView
+
+	attr_reader :data
+
+def init_data
+	@data=(1..10).map do |n|
+		element=A11y::Element.init_with_accessibility_container(self)
+		element.accessibility_test=UILabel
+		element.label="Test element #{n}"
+		element.traits=:static_text
+		element.is_accessibility_element=true
+element
+	end
+end
+
+	def accessibility_element_at_index(index)
+@data[index]
+	end
+
+	def accessibility_element_count
+		@data.count
+	end
+
+	def index_of_accessibility_element(element)
+		@data.index(element)
+	end
+
+end
+
+
